c35bc6e631db144f3fa347fa014051ea34e915b5
Eloisa <53000607+Eloise1988@users.noreply.github.com>
2022-12-12 10:37:53 -0600

    Update get_AddresswithENS.py

--- get_AddresswithENS.py
+++ get_AddresswithENS.py
@@ -4,5 +4,5 @@ from web3 import Web3
 url='https://mainnet.infura.io/v3/e567xxxxxxxxxxxxxxxxxxxxxxx'
 web3 = Web3(Web3.HTTPProvider(url))
 
-eth_address = web3.ens.address('ethereum.eth')
+eth_address = web3.ens.address(name='ethereum.eth')
 print(eth_address)
