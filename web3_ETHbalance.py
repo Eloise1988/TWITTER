311d84fa775da1e0a70b6108a5cadcd1cbcb3b1d
Eloisa <53000607+Eloise1988@users.noreply.github.com>
2022-12-20 15:22:24 -0600

    Update web3_ETHbalance.py

--- web3_ETHbalance.py
+++ web3_ETHbalance.py
@@ -1,7 +1,7 @@
 from web3 import Web3
 
 #Insert Infura API Key in URL
-url='https://mainnet.infura.io/v3/xxxxxxxxxxxxxxxxx'
+url='https://mainnet.infura.io/v3/YOUR-API-KEY'
 web3 = Web3(Web3.HTTPProvider(url))
 
 balance = web3.eth.getBalance(web3.toChecksumAddress("0x1234........r9"))
